ef016864cd59067ebcc24fcc19457b1baf5ec7a4
Francisco Carrasco <ffcarrasco@uc.cl>
2022-03-31 04:26:10 -0300

    Improving flags and debbuging them

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-from absl import flags
 from absl import app
+from Functions import Get_input_as_flags as gi
+import subprocess
 
-FLAGS = flags.FLAGS
 
-#Since Python does not have pointers, we need to define these flags as global variables
-flags.DEFINE_string('name', 'Jane Random', 'Your name.')
-flags.DEFINE_integer('age', None, 'Your age in years.', lower_bound=0)
-flags.DEFINE_boolean('debug', False, 'Produces debugging output.')
-flags.DEFINE_enum('job', 'running', ['running', 'stopped'], 'Job status.')
+# Define a class/struct with desired parameters for spaceship
+# that will be obtained as input via flags
+class Spaceship:
+    def __init__(self, length, color, style):
+        self.length = length
+        self.color = color
+        self.style = style
+
+# When a user selects some flags, she/he can enable some functions
+# with appropiated flags. This class/struct checks those options
+class UserBoolOptions:
+    def __init__(self, clipboard, help):
+        self.clipboard = clipboard
+        self.help = help
+
 
 def main(argv):
-    print('Value for name flag is {} and for integer flag is {}\n'.format(FLAGS.name, FLAGS.age))
+    # Get parameters obtained with flags in command line
+    spaceship_length, spaceship_color, spaceship_style,  user_clipboard, user_help = gi.GetInput(argv)
+
+    #Use values obtained with flags and assign them to classes/structs
+    LuanSpaceship = Spaceship(spaceship_length, spaceship_color, spaceship_style)
+    UserOptions = UserBoolOptions(user_clipboard, user_help)
+
 
 if __name__ == "__main__":
     app.run(main)
