215f40280802db15651115f91c2ae8040cc284e2
Francisco Carrasco <ffcarrasco@uc.cl>
2022-04-01 11:30:33 -0300

    Adding requirements.txt file

--- setup.py
+++ setup.py
@@ -1,27 +1,28 @@
-import setuptools
+import setuptools 
+import os
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 
+
 setuptools.setup(
         name='luanspaceship',
-        version='0.0.0',
+        version='0.0.2',
         author='Francisco Carrasco Varela',
         author_email='ffcarrasco@uc.cl',
         description='Prints an ASCII spaceship given an integer via flag \'-length\'',
         long_description=long_description,
         long_description_content_type='text/markdown',
         packages=setuptools.find_packages(),
-        install_requires=[
-            'absl-py',
-            'pyperclip'
-            ],
         classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         ],
+        install_requires=['pyperclip',
+            'absl-py'
+            ],
         entry_points='''
         [console_scripts]
         luanspaceship=LuanSpaceship.command_line:run_cli
