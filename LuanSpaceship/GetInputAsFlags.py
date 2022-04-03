d8e9923f66dd0cc96cc52394a96f76731a2994be
Francisco Carrasco <ffcarrasco@uc.cl>
2022-04-03 02:16:12 -0400

    Adding ShowColorstyles and ShowStyles functions

--- LuanSpaceship/GetInputAsFlags.py
+++ LuanSpaceship/GetInputAsFlags.py
@@ -1,5 +1,5 @@
 from absl import flags
-
+from LuanSpaceship.AddColorsAndStyles import colorstyles_list, styles_list
 
 ### Since Python does not have pointers, we need to define these flags as global
 ### variables to get their values at any function
@@ -14,6 +14,8 @@ flag_name_length = 'length'
 flag_name_clipboard = 'clipboard'
 flag_name_help = 'h'
 flag_name_print = 'print'
+flag_name_show_colorstyles = 'showcolors'
+flag_name_show_styles = 'showstyles'
 
 # Default values for flags
 flag_default_style = ''
@@ -22,6 +24,8 @@ flag_default_length = 5
 flag_default_clipboard = False
 flag_default_help = False
 flag_default_print = False
+flag_default_show_colorstyles = False
+flag_default_show_styles = False
 
 # Help text for flags
 flag_text_style = 'Select the style you want to print your ASCII Spaceship.'
@@ -30,6 +34,8 @@ flag_text_length = 'Size for Luan Spaceship (in ASCII character units (??)).'
 flag_text_clipboard = 'Copies Spaceship generated into your clipboard.'
 flag_text_help = 'Prints this help message.'
 flag_text_print = 'Prints parameters selected via flags by the user.'
+flag_text_show_colorstyles = 'Shows color styles (color combinations) availables for the program that will change ASCII spaceship color'
+flag_text_show_styles = 'Shows styles availables to add to ASCII spaceship'
 
 # Define flags
 flags.DEFINE_string(flag_name_style, flag_default_style, flag_text_style)
@@ -38,6 +44,9 @@ flags.DEFINE_integer(flag_name_length, flag_default_length, flag_text_length, lo
 flags.DEFINE_boolean(flag_name_clipboard, flag_default_clipboard, flag_text_clipboard)
 flags.DEFINE_boolean(flag_name_help, flag_default_help, flag_text_help)
 flags.DEFINE_boolean(flag_name_print, flag_default_print, flag_text_print)
+flags.DEFINE_boolean(flag_name_show_colorstyles, flag_default_show_colorstyles, flag_text_show_colorstyles)
+flags.DEFINE_boolean(flag_name_show_styles, flag_default_show_styles, flag_text_show_styles)
+
 
 
 # Get arguments via command line interface (CLI) given by the user
@@ -54,8 +63,20 @@ def checkInput(argv):
 
     if FLAGS.print:
         ShowParameters(argv)
-        
 
+    if FLAGS.showstyles or FLAGS.showcolors:
+        if FLAGS.showstyles and FLAGS.showcolors:
+            ShowColorstyles()
+            ShowStyles()
+            exit(1)
+
+        if FLAGS.showcolors:
+            ShowColorstyles()
+            exit(1)
+    
+        if FLAGS.showstyles:
+            ShowStyles()
+            exit(1)
 
 # If the user has selected '-print' flag, print parameters and their values
 def ShowParameters(argv):
@@ -64,6 +85,8 @@ def ShowParameters(argv):
     printParameter(flag_name_style, FLAGS.style)
     printParameter(flag_name_color, FLAGS.colorstyle)
     printParameter(flag_name_clipboard, FLAGS.clipboard)
+    printParameter(flag_name_show_colorstyles, FLAGS.showcolors)
+    printParameter(flag_name_show_styles, FLAGS.showstyles)
 
 
 # Print flags with their names and values selected by the user
@@ -74,6 +97,7 @@ def printParameter(name, value):
     return print("Flag: -{} {}> {}\n".format(name, '-'*quantity, value))
 
 
+# This will be printed at custom help page
 def helpLine(name, default_value, help_text):
     if default_value == '':
         default_value = '\'\''
@@ -91,3 +115,19 @@ def ShowHelp(argv):
     helpLine(flag_name_length, flag_default_length, flag_text_length)
     helpLine(flag_name_print, flag_default_print, flag_text_print)
     helpLine(flag_name_style, flag_default_style, flag_text_style)
+    helpLine(flag_name_show_colorstyles, flag_default_show_colorstyles, flag_text_show_colorstyles)
+    helpLine(flag_name_show_styles, flag_default_show_styles, flag_text_show_styles)
+
+
+# Prints color styles available
+def ShowColorstyles():
+    print("Color styles (color combinations) availables are:\n")
+    for colorstyle in colorstyles_list:
+        print("-{}\n".format(colorstyle))
+
+
+# Prints styles availables
+def ShowStyles():
+    print("Styles availables are:\n")
+    for styles in styles_list:
+        print("-{}\n".format(styles))
